175cff78e3cc1566b98fac321e8851911d034f62
David Breyault <davidbreyault@gmail.com>
2021-05-16 23:04:44 +0200

    Sauvegarde du projet efc-js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 const resultsList = document.querySelector('.results-list');
+const resultCounterElt = document.querySelector('.results-header__counter');
+const resultInfoElt = document.querySelector('.results-header__info');
 const dropdownElt = document.querySelector('.dropdown');
 const dropdownListElt = document.querySelector('.dropdown__list');
 const dropdownListItems = document.querySelectorAll('.dropdown__list--item');
 const dropdownSelectSpan = document.querySelector('.dropdown__select span');
 const dropdownSelectImg = document.querySelector('.dropdown__select img');
+const selectSpanElt = document.querySelector('.dropdown__select span');
 const searchSpanElt = document.querySelector('.search-span');
 const searchInputElt = document.querySelector('#search-input');
 const formElt = document.querySelector('.form');
-let searchType = null;
+const errorInfoElt = document.querySelector('.error-info');
+
+let parameter = null;
+
 
 function manageClass(element) {
     // Si l'argument le second paramètre est à true, ajoute cette classe, si non, supprime la.
@@ -41,13 +47,14 @@ searchInputElt.addEventListener('blur', () => {
 
 dropdownListItems.forEach((item) => {
     item.addEventListener('click', () => {
-        searchType = item.getAttribute('data-value');
-        //console.log(item.getAttribute('data-value'));
+        selectSpanElt.setAttribute('data-value', item.getAttribute('data-value'));
+        console.log(selectSpanElt.getAttribute('data-value'));
+        parameter = selectSpanElt.getAttribute('data-value');
     })
 })
 
-function createList(id, artist, title, album) {
-    // Elements creation
+function createList(id, artist, title, album, mbid) {
+    // Création des éléments
     let newId = document.createElement('p');
     let newArtist= document.createElement('p');
     let newTitle = document.createElement('p');
@@ -55,36 +62,111 @@ function createList(id, artist, title, album) {
     let newButton = document.createElement('button');
     let newImg = document.createElement('img');
     let newLine = document.createElement('li');
-    // Classes distribution
+    let newMbid = mbid;
+    // Distribution des classes
     newId.classList.add('results-list__item--id');
     newArtist.classList.add('results-list__item--artist');
     newTitle.classList.add('results-list__item--title');
     newAlbum.classList.add('results-list__item--album');
     newButton.classList.add('results-list__item--more');
     newLine.classList.add('results-list__item');
-    // Image attribute
+    // Attributs du logo
     newImg.src = 'img/logo_plus.svg';
     newImg.alt = 'logo_plus';
-    // Elements text
+    // Contenu textuel des éléments
     newId.textContent = id;
     newArtist.textContent = artist;
     newTitle.textContent = title;
     newAlbum.textContent = album;
-    // Append button element
+    // Ajout de l'image
     newButton.appendChild(newImg);
-    // Append elements
+    // Nouvelle requête lors du clique sur le bouton
+    newButton.addEventListener('click', () => {
+        console.log(id + ' mbid :  ' + newMbid);
+        lookupRequest(parameter, newMbid);
+    })
+    // Ajout des éléments
     newLine.appendChild(newId);
     newLine.appendChild(newArtist);
     newLine.appendChild(newTitle);
     newLine.appendChild(newAlbum);
     newLine.appendChild(newButton);
-    // Append this new line
+    // Ajout de la nouvelle ligne
     resultsList.insertAdjacentElement('beforeend', newLine);
 }
 
 formElt.addEventListener('submit', (e) => {
+    // Empêche le comportement par défault de la soumission d'un formulaire
     e.preventDefault();
-    let searchValue = searchInputElt.value;
+    let findSpaceRegex = /\W/;
+    // Englobe la valeur de l'input entre guillemet si l'utilisateur a rentré plusieurs mots, afin de filtrer plus précisément les résultats de la requête
+    let inputValue = findSpaceRegex.test(searchInputElt.value) ? '\"' + searchInputElt.value + '\"' : searchInputElt.value;
+    // Si le champs input n'est pas vide
+    if (inputValue.length > 0) {
+        // Si l'utilisateur a sélectionné un type de recherche
+        if (selectSpanElt.hasAttribute('data-value')) {
+            // Montre ou cache les éléments d'informations
+            errorInfoElt.textContent = '';
+            resultCounterElt.style.display = 'block';
+            resultInfoElt.style.display = 'none';
+            // Vide resultList a chaque soumission de formulaire pour afficher les nouveaux résultats de la requête
+            while (resultsList.hasChildNodes()) {
+                resultsList.firstChild.remove();
+            }
+            if (selectSpanElt.getAttribute('data-value') === 'all') {
+                searchAllTypesRequest(encodeURIComponent(inputValue));
+            } else {
+                searchRequest(parameter, encodeURIComponent(inputValue));
+            }
+            console.log(encodeURIComponent(inputValue));
+            console.log(inputValue);
+        } else {
+            errorInfoElt.textContent = 'Please select your type of search.';
+        }
+    } else {
+        errorInfoElt.textContent = 'Please enter something to search.';
+    }
+})
+
+function searchRequest(parameter, value) {
+    let request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+        // Si l'opération de récupération est terminée
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Si la requête a été effectué avec succès
+            if (request.status === 200) {
+                // Conversion des données JSON en données interprétables par le Javascript
+                let response = JSON.parse(request.response);
+                console.log(response);
+                // Affiche le nombre de résultats trouvés
+                resultCounterElt.textContent = response.count + ' results';
+                // Si aucun résultat n'est trouvé
+                if (response.count === 0) {
+                    // Affiche les infos a l'utilisateur
+                    resultInfoElt.style.display = 'block';
+                    resultInfoElt.textContent = 'There is no result for your request.';
+                } else {
+                    // Si non lance la fonction createList pour chaque éléments du tableau recordings 
+                    response.recordings.forEach((element, index) => {
+                        createList(
+                            index + 1,
+                            element['artist-credit'][0].name,
+                            element.title,
+                            element.releases ? element.releases[0].title : '',
+                            element.id
+                        );
+                    });
+                }
+            } else {
+                console.error('Error !');
+            }
+        }
+    })
+    request.open('GET', `http://musicbrainz.org/ws/2/recording/?query=${parameter}:${value}&fmt=json&limit=100&offset=0`);
+    request.send();
+}
+
+function searchAllTypesRequest(value) {
     let request = new XMLHttpRequest();
     request.addEventListener('readystatechange', () => {
         //console.log(request.readyState); 
@@ -93,40 +175,61 @@ formElt.addEventListener('submit', (e) => {
                 // Conversion des données JSON en données interprétables par le Javascript
                 let response = JSON.parse(request.response);
                 console.log(response);
-                
+                resultCounterElt.textContent = response.count + ' results';
+                response.recordings.forEach((element, index) => {
+                    createList(
+                        index + 1,
+                        element['artist-credit'][0].name,
+                        element.title,
+                        element.releases ? element.releases[0].title : '',
+                        element.id
+                    );
+                });
             } else {
                 console.error('Error !');
             }
         }
     })
-
-    request.open('GET', `https://musicbrainz.org/ws/2/${searchType}?query=${searchValue}&fmt=json`);
+    request.open('GET', `http://musicbrainz.org/ws/2/recording/?query=release:${value}artist:${value}recording:${value}&fmt=json&limit=100&offset=0`);
     request.send();
-})
+}
 
+//searchAllTypesRequest();
 
-/*
-let request = new XMLHttpRequest();
+function lookupRequest(mbid) {
+    let request = new XMLHttpRequest();
     request.addEventListener('readystatechange', () => {
-        //console.log(request.readyState); 
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
                 // Conversion des données JSON en données interprétables par le Javascript
                 let response = JSON.parse(request.response);
                 console.log(response);
-                
             } else {
                 console.error('Error !');
             }
         }
     })
-//request.open('GET', 'https://musicbrainz.org/ws/2/recording?query=%22we%20will%20rock%20you%22%20AND%20arid:0383dadf-2a4e-4d10-a46a-e9e041da8eb3&fmt=json');
-//request.open('GET', `https://musicbrainz.org/ws/2/release?query=${encodeURIComponent('Miranda Shvangiradze')}&fmt=json&inc=recordings+artist`);
-request.open('GET', `https://musicbrainz.org/ws/2/artist?query=odezenne&fmt=json`);
-request.send();
-*/
+    request.open('GET', `https://musicbrainz.org/ws/2/recording/${mbid}?inc=artists+releases&fmt=json`);
+    request.send(); 
+}
 
+function lookupRequestTest(parameter, mbid) {
+    let request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                // Conversion des données JSON en données interprétables par le Javascript
+                let response = JSON.parse(request.response);
+                console.log(response);
+            } else {
+                console.error('Error !');
+            }
+        }
+    })
+    request.open('GET', `https://musicbrainz.org/ws/2/${parameter}/${mbid}?inc=artists+releases+discids+ratings&fmt=json`);
+    request.send(); 
+}
 
-
+//lookupRequestTest('recording', '27af3d46-6f52-456d-96cb-b18ad379a939');
 
 
---
+++ js/scroll-top.js
@@ -0,0 +1,14 @@
+const btnScrollToTop = document.querySelector('#btn-scroll-top');
+
+window.addEventListener('scroll', () => {
+    btnScrollToTop.style.display = window.pageYOffset >= 400 ? 'block' : 'none';
+})
+
+btnScrollToTop.addEventListener('click', (e) => {
+    window.scrollTo({
+        top:00,
+        left:0,
+        behavior:"smooth"
+    });
+    console.log(e.target);
+})
